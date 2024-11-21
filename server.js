81322643e1853efa7666e116de3a8fb6bc1d6191
Victtor Marcelo <victtordesousa1@gmail.com>
2024-11-20 22:46:07 -0300

    aula-02

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 import express from "express";
 
+const posts = [
+    {
+      id: 1,
+      descricao: "Um gato surfista radical pegando uma onda imaginária!",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 2,
+      descricao: "Felix está pronto para estrelar um comercial de ração, com aquele sorriso irresistível!",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 3,
+      descricao: "Whiskers compôs uma nova ópera e está prestes a estreá-la para a plateia de passarinhos!",
+      imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Servidor escutando.......");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão Dev da Alura com Google Gemini!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
 });
